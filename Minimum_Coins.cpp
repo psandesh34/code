f0011b6cbf4c0f97f5fd5ff6517f9c463ade298c
sandesh <psandesh01.sp@gmail.com>
2022-07-19 23:31:08 +0530

    19th July 2022: Minimum coins

---
+++ Minimum_Coins.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+#define endl "\n"
+#define ll long long
+#define sd(val) scanf("%d", &val)
+#define ss(val) scanf("%s", &val)
+#define sf(val) scanf("%f", &val)
+#define sl(val) scanf("%lld", &val)
+#define print(val) cout<<val<<endl;
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define _for(i, j, k, in) for (int i = j; i < k; i += in)
+#define _rfor(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define rep(j, k) for (int i = j; i < k; i++)
+#define rrep(j, k) for (int i = j; i < k; i--)
+
+int main()
+{
+    FIO;
+    int t;
+    sd(t);
+    while (t--)
+    {
+        int n;
+        sd(n);
+        print(n%10);
+    }
+    return 0;
+}
